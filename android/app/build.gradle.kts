939c265b49fca4ec26699cebcb9020950eaa26aa
yassnath <andreasnath04@gmail.com>
2026-02-12 12:15:32 +0700

    fixing UI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 
 
 dependencies {
-  val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
+  val composeBom = platform("androidx.compose:compose-bom:2025.09.01")
   implementation(composeBom)
   androidTestImplementation(composeBom)
 
